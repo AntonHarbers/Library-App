90f741c7ab7bc5092de670a3cfc9a9e8cadb7a5b
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2023-07-04 19:11:43 +0400

    Final Commit for now

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const formButton = document.querySelector('#formButton');
-const toggleFormButton = document.querySelector('#toggleFormButton');
-const form = document.querySelector('form');
-const closeFormButton = document.querySelector('#closeFormButton');
+const formButton = document.querySelector("#formButton");
+const toggleFormButton = document.querySelector("#toggleFormButton");
+const form = document.querySelector("form");
+const closeFormButton = document.querySelector("#closeFormButton");
+const bookshelf = document.getElementById("bookshelf");
 
 let myLibrary = [];
+
+document.addEventListener('scroll', function() {
+  const parallax = document.querySelector('.parallax');
+  let scrollPosition = window.pageYOffset;
+  parallax.style.backgroundPosition = '0' + ' ' + (scrollPosition * 0.5) + 'px';
+});
 
 function Book(title, author, pages, read) {
   (this.title = title),
@@ -12,8 +19,8 @@ function Book(title, author, pages, read) {
     (this.read = read);
 
   this.info = () => {
-    return `${title} by ${author}, ${pages} pages, ${
-      read ? 'read' : 'not read yet'
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "read" : "not read yet"
     }`;
   };
 }
@@ -23,65 +30,94 @@ function addBookToLibrary(book) {
 }
 
 function paintBookshelf() {
-  document.getElementById('bookshelf').innerHTML = '';
-  for (var i = 0; i < myLibrary.length; i++) {
-    var element = document.createElement('div');
+  bookshelf.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) { 
+    let element = document.createElement("div");
     element.dataset.index = i;
-    element.classList.add('book');
-    element.innerHTML = myLibrary[i].info();
-    element.innerHTML += `<button class="removeButton">X</button>`;
-    document.getElementById('bookshelf').appendChild(element);
+    element.classList.add("book");
+
+    let infoElement = document.createElement("div");
+    infoElement.textContent = myLibrary[i].info();
+    element.appendChild(infoElement);
+
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer");
+    element.appendChild(buttonContainer);
+
+    let readToggleButton = document.createElement("button");
+    readToggleButton.classList.add("ReadToggleButton");
+    readToggleButton.textContent = "Toggle Read";
+    readToggleButton.addEventListener("click", (e) => {
+      // Toggle the read status
+      myLibrary[i].read = !myLibrary[i].read;
+
+      // Update the text content with the new info
+      infoElement.textContent = myLibrary[i].info();
+    });
+    buttonContainer.appendChild(readToggleButton);
+
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("RemoveButton");
+    removeButton.textContent = "X";
+    removeButton.addEventListener("click", (e) => {
+      myLibrary.splice(i, 1);
+      paintBookshelf();
+    });
+    buttonContainer.appendChild(removeButton);
+    
+    bookshelf.appendChild(element);
   }
 }
 
 paintBookshelf();
 
-closeFormButton.addEventListener('click', () => {
-    form.classList.toggle('hidden');
-    form.reset();
-})
+closeFormButton.addEventListener("click", () => {
+  form.classList.toggle("hidden");
+  form.reset();
+});
 
-formButton.addEventListener('click', (e) => {
-  //   form.classList.toggle('hidden');
+toggleFormButton.addEventListener("click", () => {
+  form.classList.toggle("hidden");
+});
+
+formButton.addEventListener("click", (e) => {
   e.preventDefault();
-  var formData = new FormData(form); // Create a FormData object to store form data
+  var formData = new FormData(form); 
 
-  console.log(formData.get('name'));
-  console.log(formData.get('author'));
-  console.log(formData.get('pages'));
-  console.log(formData.get('read') == null ? false : true);
+  console.log(formData.get("name"));
+  console.log(formData.get("author"));
+  console.log(formData.get("pages"));
+  console.log(formData.get("read") == null ? false : true);
 
-  if (formData.get('name') === '') {
-    alert('Please enter a book name');
+  if (formData.get("name") === "") {
+    alert("Please enter a book name");
     return;
   }
 
-  if (formData.get('author') === '') {
-    alert('Please enter an author name');
+  if (formData.get("author") === "") {
+    alert("Please enter an author name");
     return;
   }
 
-  if (formData.get('pages') === '0') {
-    alert('Please enter a page number');
+  if (formData.get("pages") === "0") {
+    alert("Please enter a page number");
     return;
   }
 
   addBookToLibrary(
     new Book(
-      formData.get('name'),
-      formData.get('author'),
-      formData.get('pages'),
-      formData.get('read') == null ? false : true
+      formData.get("name"),
+      formData.get("author"),
+      formData.get("pages"),
+      formData.get("read") == null ? false : true
     )
   );
 
   form.reset();
 
-  form.classList.toggle('hidden');
+  form.classList.toggle("hidden");
 
   paintBookshelf();
 });
 
-toggleFormButton.addEventListener('click', () => {
-  form.classList.toggle('hidden');
-});
+
